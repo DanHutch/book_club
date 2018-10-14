77c1958fc682cc4df9e78287d150146e05cb528e
DanHutch <danhutch5280@gmail.com>
2018-10-13 19:33:34 -0600

    wrote tests and implemented code for user story 10, all tests passing

--- spec/features/author_show_spec.rb
+++ spec/features/author_show_spec.rb
@@ -10,6 +10,15 @@ describe "author_show" do
 
     book_2 = Book.create(title: "Dan's second Book", pages: 100, year: 2017, authors: [author])
 
+    user_1 = User.create(name: "Review Master")
+    user_2 = User.create(name: "Your Mom")
+
+    review_1 = user_1.reviews.create(title: "These Tests Are Gonna Pass", description: "I've got a great feeling about this!", score: 5, book: book_1)
+    review_2 = user_2.reviews.create(title: "You Shall Pass", description: "I've got a pretty good feeling about this!", score: 4, book: book_1)
+
+    review_3 = user_1.reviews.create(title: "This is a review", description: "You've got a book.", score: 3, book: book_2)
+    review_4 = user_2.reviews.create(title: "Whatevs", description: "blah blah blah", score: 2, book: book_2)
+
 
     visit "authors/#{author.id}"
 
@@ -19,10 +28,17 @@ describe "author_show" do
         expect(all('.author_book_card')[0]).to have_content("#{book_1.title}")
         expect(all('.author_book_card')[0]).to have_content("#{book_1.pages}")
         expect(all('.author_book_card')[0]).to have_content("#{author_2.name}")
+        expect(all('.author_book_card')[0]).to have_content("Top Review: #{review_1.title}")
+        expect(all('.author_book_card')[0]).to have_content("Score: #{review_1.score}")
+        expect(all('.author_book_card')[0]).to have_content("Reviewed By: #{review_1.user.name}")
+        expect(all('.author_book_card')[0]).to_not have_content("Reviewed By: #{review_2.user.name}")
 
         expect(all('.author_book_card')[1]).to have_content("#{book_2.title}")
         expect(all('.author_book_card')[1]).to have_content("#{book_2.pages}")
-
+        expect(all('.author_book_card')[1]).to have_content("Top Review: #{review_3.title}")
+        expect(all('.author_book_card')[1]).to have_content("Score: #{review_3.score}")
+        expect(all('.author_book_card')[1]).to have_content("Reviewed By: #{review_3.user.name}")
+        expect(all('.author_book_card')[0]).to_not have_content("Top Review: #{review_4.title}")
       end
 
   end
