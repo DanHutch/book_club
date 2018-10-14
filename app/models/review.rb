a4f8b8228ab1991766e512825c416e9e8e6b609e
Cesar Jolibois <diazcd85@gmail.com>
2018-10-14 00:20:17 -0600

    WIP keep breaking stuff

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ApplicationRecord
-  validates_presence_of :title, :description, :score
+  validates_presence_of :title, :description
+
+  validates_numericality_of :score, presence: true, numericality: {
+    only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
   belongs_to :book
   belongs_to :user
