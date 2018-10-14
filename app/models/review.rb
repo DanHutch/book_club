c6217c97262b32302fbb2071084381f3c76270d8
Cesar Jolibois <diazcd85@gmail.com>
2018-10-14 14:13:25 -0600

    update review model test to ensure that score is an integer between 1  to 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
-  validates_presence_of :title, :description
+  validates_presence_of :title, :description, :score
 
-  validates_numericality_of :score, presence: true, numericality: {
+  validates :score, numericality: {
     only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
   belongs_to :book
