971ed3a2d1bb07c5c7cd713887972458c79586ad
DanHutch <danhutch5280@gmail.com>
2018-10-15 11:39:56 -0600

    initial refactor commit to controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :books, :except => [:edit, :update]
-  resources :authors, :except => [:index, :edit, :update ]
+
+  resources :books, :only => [:index, :show, :create, :new]
+  resources :authors, :only => [:show, :create]
   resources :reviews, :only => [:new, :destroy, :create]
   resources :users, :only => [:show, :new, :create]
 
