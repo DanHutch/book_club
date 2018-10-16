ef0ccbf39f2a475ced15d3f079490d7081444c41
Cesar Jolibois <diazcd85@gmail.com>
2018-10-16 16:39:10 -0600

    create test for deleting author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,6 +4,14 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-
+  def destroy
+    @author = Author.find(params[:id])
+    books = @author.books
+    books.each do |book|
+      book.destroy if book.authors.count < 2
+    end
+    @author.destroy
+    redirect_to books_path
+  end
 
 end
